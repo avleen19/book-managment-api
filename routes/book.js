6b161bf73d7dd35adf9052372db4af28125bed96
avleen19 <123055235+avleen19@users.noreply.github.com>
2024-09-11 22:27:03 +0530

    Update book.js

--- routes/book.js
+++ routes/book.js
@@ -4,15 +4,13 @@ const bookController = require('../controllers/bookController');
 const basicAuth = require('../middlewares/authmiddleware');
 
 // Public routes
+router.get('/search', bookController.searchBooks); // Move this above the dynamic /:id route
 router.get('/', bookController.getAllBooks);
 router.get('/:id', bookController.getBookById);
-
 
 // Protected routes
 router.post('/', basicAuth, bookController.createBook);
 router.put('/:id', basicAuth, bookController.updateBook);
 router.delete('/:id', basicAuth, bookController.deleteBook);
-
-router.get('/search', bookController.searchBooks);
 
 module.exports = router;
